67b4e1d95b64c4db23dfa49ff4343598fe50fc34
young8Programmer <valireyimbergenov79@gmail.com>
2026-01-18 00:37:20 +0500

    chore: CI/CD pipeline sozlandi

--- src/lessons/dto/update-lesson.dto.ts
+++ src/lessons/dto/update-lesson.dto.ts
@@ -1,5 +1,6 @@
 // API response formatini yaxshilash
 import { PartialType } from '@nestjs/mapped-types';
+// prettier formatlash
 import { CreateLessonDto } from './create-lesson.dto';
 
 export class UpdateLessonDto extends PartialType(CreateLessonDto) {}
