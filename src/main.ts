25c362c2fd926b65f90a4a6a6fa60d372e5ac984
young8Programmer <valireyimbergenov79@gmail.com>
2026-01-18 00:37:37 +0500

    perf: image optimization qo'shildi

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,7 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   await app.listen(process.env.PORT ?? 3000);
+// API endpoint testlari qo'shildi
 // database querylarni optimallashtirish
 }
 
