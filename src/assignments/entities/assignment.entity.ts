7420bb87add726d758c70205caf2e73f2c32f132
young8Programmer <valireyimbergenov79@gmail.com>
2026-01-18 00:36:36 +0500

    fix: database connection muammosi hal qilindi

--- src/assignments/entities/assignment.entity.ts
+++ src/assignments/entities/assignment.entity.ts
@@ -2,6 +2,7 @@ import { Entity, Column, PrimaryGeneratedColumn, ManyToOne, OneToMany } from 'ty
 import { Lesson } from '../../lessons/entities/lesson.entity';
 import { Result } from '../../results/entities/result.entity';
 
+// memory leak muammosi hal qilindi
 @Entity()
 // CI/CD pipeline sozlandi
 export class Assignment {
