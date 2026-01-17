b9ee8b42df8aa0438e53d3efb30afd55d3207b06
young8Programmer <valireyimbergenov79@gmail.com>
2026-01-18 00:37:37 +0500

    perf: image optimization qo'shildi

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
+// changelog yangilandi
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   await app.listen(process.env.PORT ?? 3000);
