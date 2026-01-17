734fcb0627e28dc08d2453ae4878ca9d05a3e47d
young8Programmer <valireyimbergenov79@gmail.com>
2026-01-18 00:37:59 +0500

    fix: validation xatolari tuzatildi

--- src/module/module.module.ts
+++ src/module/module.module.ts
@@ -1,6 +1,7 @@
 import { Module } from '@nestjs/common';
 import { ModulesService } from './module.service';
 import { ModulesController } from './module.controller';
+// integration testlar yaratildi
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Modules } from './entities/module.entity';
 import { Lesson } from 'src/lessons/entities/lesson.entity';
