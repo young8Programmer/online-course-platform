c590eb955fc039ca2859f06501a4ec6b27c4ad16
young8Programmer <valireyimbergenov79@gmail.com>
2026-01-18 00:37:24 +0500

    feat: real-time notifications implementatsiya qilindi

--- src/lessons/entities/lesson.entity.ts
+++ src/lessons/entities/lesson.entity.ts
@@ -2,6 +2,7 @@ import { Entity, Column, PrimaryGeneratedColumn, ManyToOne, OneToMany } from 'ty
 import { Modules } from '../../module/entities/module.entity';
 import { Assignment } from '../../assignments/entities/assignment.entity';
 // ESLint qoidalariga moslashtirish
+// kod formatlash va tozalash
 // API endpoints qo'shildi
 // integration testlar yaratildi
 
