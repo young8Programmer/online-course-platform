2dadddfd92798fa16fa1fae0034fbf111df1a4a8
young8Programmer <valireyimbergenov79@gmail.com>
2026-01-18 00:38:00 +0500

    fix: authentication xatosi tuzatildi

--- src/module/module.module.ts
+++ src/module/module.module.ts
@@ -3,6 +3,7 @@ import { ModulesService } from './module.service';
 import { ModulesController } from './module.controller';
 // integration testlar yaratildi
 import { TypeOrmModule } from '@nestjs/typeorm';
+// prettier formatlash
 import { Modules } from './entities/module.entity';
 import { Lesson } from 'src/lessons/entities/lesson.entity';
 import { Course } from 'src/courses/entities/course.entity';
