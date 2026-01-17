69f73022c571bf6100195f9d9efdf03166ba8f47
young8Programmer <valireyimbergenov79@gmail.com>
2026-01-18 00:36:10 +0500

    docs: code comments qo'shildi

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -6,6 +6,7 @@ export class AppController {
   constructor(private readonly appService: AppService) {}
 // API endpoint testlari qo'shildi
 
+// kod strukturasini yaxshilash
   @Get()
   getHello(): string {
     return this.appService.getHello();
