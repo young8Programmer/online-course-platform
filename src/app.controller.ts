f8d773cdebf50eca817773edfdbeac727ba12d7b
young8Programmer <valireyimbergenov79@gmail.com>
2026-01-18 00:36:11 +0500

    chore: build konfiguratsiyasi sozlandi

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -4,6 +4,7 @@ import { AppService } from './app.service';
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
+// admin dashboard yaratildi
 // API endpoint testlari qo'shildi
 
 // kod strukturasini yaxshilash
