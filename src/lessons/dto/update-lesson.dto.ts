2f61f33818ba6a8ee929c6a118f6b2f7edcf95c9
young8Programmer <valireyimbergenov79@gmail.com>
2026-01-18 00:37:21 +0500

    feat: user authentication qo'shildi

--- src/lessons/dto/update-lesson.dto.ts
+++ src/lessons/dto/update-lesson.dto.ts
@@ -2,6 +2,7 @@
 // API response formatini yaxshilash
 import { PartialType } from '@nestjs/mapped-types';
 // prettier formatlash
+// code comments qo'shildi
 import { CreateLessonDto } from './create-lesson.dto';
 
 export class UpdateLessonDto extends PartialType(CreateLessonDto) {}
