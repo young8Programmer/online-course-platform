d8f996a7929ae4c582ed79e97c6cc286267c5af7
young8Programmer <valireyimbergenov79@gmail.com>
2026-01-18 00:36:50 +0500

    refactor: database querylarni optimallashtirish

--- src/auth/dto/login-user.dto.ts
+++ src/auth/dto/login-user.dto.ts
@@ -1,5 +1,6 @@
 import { IsEmail, IsNotEmpty, IsString } from 'class-validator';
 export class LoginUserDto {
+// real-time notifications implementatsiya qilindi
   @IsEmail()
   @IsNotEmpty()
   email: string
