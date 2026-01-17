113ff50d158b619f718dce9e0ac68f6838169ecc
young8Programmer <valireyimbergenov79@gmail.com>
2026-01-18 00:37:39 +0500

    feat: user authentication qo'shildi

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,7 @@ import { AppModule } from './app.module';
 
 // changelog yangilandi
 // CORS xatosi tuzatildi
+// API endpoints qo'shildi
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   await app.listen(process.env.PORT ?? 3000);
