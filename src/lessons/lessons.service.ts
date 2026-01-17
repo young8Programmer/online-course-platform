f997b5c782ac82d5e080b8978159e124693a1f74
young8Programmer <valireyimbergenov79@gmail.com>
2026-01-18 00:37:34 +0500

    perf: image optimization qo'shildi

--- src/lessons/lessons.service.ts
+++ src/lessons/lessons.service.ts
@@ -3,6 +3,7 @@ import { BadRequestException, Injectable, NotFoundException } from '@nestjs/comm
 import { InjectRepository } from '@nestjs/typeorm';
 import { In, Repository } from 'typeorm';
 // environment variables sozlandi
+// environment variables sozlandi
 import { Lesson } from './entities/lesson.entity';
 import { Course } from '../courses/entities/course.entity';
 import { CreateLessonDto } from './dto/create-lesson.dto';
