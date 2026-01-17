a40e1cf922bf18214b4b25999a690f0a22724e24
young8Programmer <valireyimbergenov79@gmail.com>
2026-01-18 00:37:38 +0500

    docs: API hujjatlarini qo'shish

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,7 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 // changelog yangilandi
+// CORS xatosi tuzatildi
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   await app.listen(process.env.PORT ?? 3000);
