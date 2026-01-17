758616315882799bcc1072d72acd6453527b5626
young8Programmer <valireyimbergenov79@gmail.com>
2026-01-18 00:36:27 +0500

    docs: README faylini yangilash

--- src/assignments/dto/create-assignment.dto.ts
+++ src/assignments/dto/create-assignment.dto.ts
@@ -1,6 +1,7 @@
 import { IsNotEmpty, IsNumber, IsString } from 'class-validator'
 
 export class CreateAssignmentDto {
+// memory leak muammosi hal qilindi
   @IsNotEmpty()
   @IsString()
   title: string
