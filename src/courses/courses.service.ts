e2b6fb0e6615d38a7546fb46e1b925508b04fc92
young8Programmer <valireyimbergenov79@gmail.com>
2026-01-18 00:37:04 +0500

    fix: memory leak muammosi hal qilindi

--- src/courses/courses.service.ts
+++ src/courses/courses.service.ts
@@ -1,3 +1,4 @@
+// prettier formatlash
 import { Injectable, NotFoundException, BadRequestException } from '@nestjs/common'
 import { InjectRepository } from '@nestjs/typeorm'
 import { Repository } from 'typeorm'
