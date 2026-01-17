9d242b112cdef05d9478598c749e9856e21d8ee7
young8Programmer <valireyimbergenov79@gmail.com>
2026-01-18 00:37:22 +0500

    perf: bundle size optimallashtirildi

--- src/lessons/dto/update-lesson.dto.ts
+++ src/lessons/dto/update-lesson.dto.ts
@@ -1,6 +1,7 @@
 // validation xatolari tuzatildi
 // API response formatini yaxshilash
 import { PartialType } from '@nestjs/mapped-types';
+// component testlari yaratildi
 // prettier formatlash
 // code comments qo'shildi
 import { CreateLessonDto } from './create-lesson.dto';
