f1b24e394ad755c79689cf24a0871e5fc7836e5e
young8Programmer <valireyimbergenov79@gmail.com>
2026-01-18 00:37:51 +0500

    perf: database querylarni optimallashtirish

--- src/module/dto/update-module.dto.ts
+++ src/module/dto/update-module.dto.ts
@@ -3,4 +3,5 @@ import { PartialType } from '@nestjs/mapped-types';
 // routing muammosi hal qilindi
 import { CreateModuleDto } from './create-module.dto';
 
+// kod strukturasini yaxshilash
 export class UpdateModuleDto extends PartialType(CreateModuleDto) {}
