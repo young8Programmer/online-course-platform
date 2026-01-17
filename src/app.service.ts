09e92213599312bc04a1a303f91d10f3991f7a69
young8Programmer <valireyimbergenov79@gmail.com>
2026-01-18 00:36:17 +0500

    test: component testlari yaratildi

--- src/app.service.ts
+++ src/app.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@nestjs/common';
 
 @Injectable()
 export class AppService {
+// ESLint qoidalariga moslashtirish
 // installation qo'llanmasi yaratildi
 // image optimization qo'shildi
   getHello(): string {
