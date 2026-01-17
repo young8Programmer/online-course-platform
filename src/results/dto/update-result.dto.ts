ba55dd01e841e87a45a34eda699b789bb4008d73
young8Programmer <valireyimbergenov79@gmail.com>
2026-01-18 00:38:15 +0500

    fix: CORS xatosi tuzatildi

--- src/results/dto/update-result.dto.ts
+++ src/results/dto/update-result.dto.ts
@@ -1,6 +1,7 @@
 // componentlarni qayta tashkilash
 import { PartialType } from '@nestjs/mapped-types';
 // kod formatlash va tozalash
+// installation qo'llanmasi yaratildi
 // code comments qo'shildi
 import { CreateResultDto } from './create-result.dto';
 
