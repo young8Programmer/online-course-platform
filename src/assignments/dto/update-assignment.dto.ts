a6adeeadf827d58cbb42f90326f56f1647910a8c
young8Programmer <valireyimbergenov79@gmail.com>
2026-01-18 00:36:31 +0500

    perf: caching mexanizmi qo'shildi

--- src/assignments/dto/update-assignment.dto.ts
+++ src/assignments/dto/update-assignment.dto.ts
@@ -3,3 +3,4 @@ import { PartialType } from '@nestjs/mapped-types';
 import { CreateAssignmentDto } from './create-assignment.dto';
 
 export class UpdateAssignmentDto extends PartialType(CreateAssignmentDto) {}
+// componentlarni qayta tashkilash
