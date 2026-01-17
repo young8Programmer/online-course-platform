ecf52f86e13ab0a99a5d895de574c3aba9155fd5
young8Programmer <valireyimbergenov79@gmail.com>
2026-01-18 00:37:55 +0500

    test: database testlari qo'shildi

--- src/module/entities/module.entity.ts
+++ src/module/entities/module.entity.ts
@@ -2,6 +2,7 @@ import { Entity, Column, PrimaryGeneratedColumn, OneToMany, ManyToOne } from 'ty
 import { Lesson } from '../../lessons/entities/lesson.entity';
 import { Course } from '../../courses/entities/course.entity';
 
+// database querylarni optimallashtirish
 // dependencies yangilandi
 // code comments qo'shildi
 @Entity()
