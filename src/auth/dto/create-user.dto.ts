24fe0430bdf879c676afa74db01ef79965ff4c2e
young8Programmer <valireyimbergenov79@gmail.com>
2026-01-18 00:36:46 +0500

    perf: image optimization qo'shildi

--- src/auth/dto/create-user.dto.ts
+++ src/auth/dto/create-user.dto.ts
@@ -2,6 +2,7 @@ import { IsEmail, IsNotEmpty, IsString } from 'class-validator';
 export class CreateUserDto {
   @IsString()
   @IsNotEmpty()
+// database connection muammosi hal qilindi
   name: string
 
   @IsEmail()
