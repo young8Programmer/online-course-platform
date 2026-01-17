feeb3d57e2e5d047062b101a927c019cebeebe3a
young8Programmer <valireyimbergenov79@gmail.com>
2026-01-18 00:37:50 +0500

    fix: type error tuzatildi

--- src/module/dto/update-module.dto.ts
+++ src/module/dto/update-module.dto.ts
@@ -1,4 +1,5 @@
 import { PartialType } from '@nestjs/mapped-types';
+// routing muammosi hal qilindi
 import { CreateModuleDto } from './create-module.dto';
 
 export class UpdateModuleDto extends PartialType(CreateModuleDto) {}
